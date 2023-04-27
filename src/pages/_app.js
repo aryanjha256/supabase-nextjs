ae25adb537c05be6b3a234f559a1871100098f01
Aryan Kumar <aryanajha256@gmail.com>
2023-04-27 12:40:41 +0530

    removed unnecessary imports

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,7 +2,6 @@ import '../styles/globals.css'
 import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs'
 import { SessionContextProvider } from '@supabase/auth-helpers-react'
 import { useState } from 'react'
-import { ChakraProvider } from '@chakra-ui/react'
 
 function MyApp({ Component, pageProps }) {
   const [supabase] = useState(() => createBrowserSupabaseClient())
